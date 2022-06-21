059129c9edc46d34ba9d49075b86cbad19e26fd6
navyajain16 <72208843+navyajain16@users.noreply.github.com>
2022-06-22 01:59:18 +0530

    Delete MPRNet/pretrained models directory

--- MPRNet/pretrained models/__init__.py
+++
@@ -1,4 +0,0 @@
-from .dir_utils import mkdirs,mkdir,get_last_path
-from .image_utils import torchPSNR,save_img,numpyPSNR
-from .model_utils import save_checkpoint,load_checkpoint,load_checkpoint_multigpu,load_start_epoch,load_optim
-from .dataset_utils import MixUp_AUG
