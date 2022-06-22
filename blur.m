1ffe233e01a5f22eca9f3e651c8cf319a1c6ffba
navyajain16 <72208843+navyajain16@users.noreply.github.com>
2022-06-22 12:37:53 +0530

    Update blur.m

--- blur.m
+++ blur.m
@@ -2,7 +2,7 @@
 I = dir(['C:\Evaluation\SWIMCAT,*.png]);
 
 %Create a PSF that represents a Gaussian blur with standard deviation 5 and filter of size 5-by-5         
-PSF = fspecial('gaussian',5,5);PSF = fspecial('gaussian',5,5);         
+PSF = fspecial('gaussian',5,5);         
 
 %Simulate blur in the image.
 blurred = imfilter(I,PSF,'symmetric','conv');   
